feaa4602659dfb869c3d35d34388f18ef6c14823
Bomin Chuang <appppm31012@gmail.com>
2020-01-02 15:29:11 +0800

    Add Algorithm

---
+++ Algorithm/src/BST_Delete.java
@@ -0,0 +1,139 @@
+public class BST_Delete{
+
+    class Node { 
+        int key; 
+        Node left, right; 
+  
+        public Node(int item){ 
+            key = item; 
+            left = right = null; 
+        } 
+    } 
+  
+    // Root of BST 
+    Node root; 
+  
+    // Constructor 
+    BST_Delete(){ 
+        root = null; 
+    } 
+  
+    void deleteKey(int key){ 
+        root = deleteRec(root, key); 
+    } 
+  
+    Node deleteRec(Node root, int key) { 
+        /* Base Case: If the tree is empty */
+        if (root == null)  return root; 
+  
+       //Traversal
+        if (key < root.key) 
+            root.left = deleteRec(root.left, key); 
+        else if (key > root.key) 
+            root.right = deleteRec(root.right, key); 
+  
+        // if key is same as root's key, then This is the node to be deleted
+        else
+        { 
+            // node with only one child or no child 
+            if (root.left == null) 
+                return root.right; 
+            else if (root.right == null) 
+                return root.left; 
+  
+            // node with two children: Get the inorder successor (smallest in the right subtree) 
+            root.key = minValue(root.right); 
+  
+            // Delete the inorder successor 
+            root.right = deleteRec(root.right, root.key); 
+        } 
+  
+        return root; 
+    } 
+    
+    //Find the min of left node
+    int minValue(Node root) 
+    { 
+        int minv = root.key; 
+        while (root.left != null) 
+        { 
+            minv = root.left.key; 
+            root = root.left; 
+        } 
+        return minv; 
+    } 
+  
+    void insert(int key) { 
+        root = insertRec(root, key); 
+    } 
+  
+
+    Node insertRec(Node root, int key) { 
+  
+        if (root == null) 
+        { 
+            root = new Node(key); 
+            return root; 
+        } 
+  
+        /* Otherwise, recur down the tree */
+        if (key < root.key) 
+            root.left = insertRec(root.left, key); 
+        else if (key > root.key) 
+            root.right = insertRec(root.right, key); 
+  
+        /* return the (unchanged) node pointer */
+        return root; 
+    } 
+  
+    void inorder() 
+    { 
+        inorderRec(root); 
+    } 
+  
+    void inorderRec(Node root) { 
+        if (root != null) 
+        { 
+            inorderRec(root.left); 
+            System.out.print(root.key + " "); 
+            inorderRec(root.right); 
+        } 
+    } 
+  
+    public static void main(String[] args) { 
+    	BST_Delete tree = new BST_Delete(); 
+  
+        /*  
+              50 
+           /     \ 
+          30      70 
+         /  \    /  \ 
+        20   40  60   80 */
+    	
+        tree.insert(50); 
+        tree.insert(30); 
+        tree.insert(20); 
+        tree.insert(40); 
+        tree.insert(70); 
+        tree.insert(60); 
+        tree.insert(80); 
+  
+        System.out.println("Inorder traversal of the given tree"); 
+        tree.inorder(); 
+  
+        System.out.println("\nDelete 20"); 
+        tree.deleteKey(20); 
+        System.out.println("Inorder traversal of the modified tree"); 
+        tree.inorder(); 
+  
+        System.out.println("\nDelete 30"); 
+        tree.deleteKey(30); 
+        System.out.println("Inorder traversal of the modified tree"); 
+        tree.inorder(); 
+  
+        System.out.println("\nDelete 50"); 
+        tree.deleteKey(50); 
+        System.out.println("Inorder traversal of the modified tree"); 
+        tree.inorder(); 
+    } 
+} 
